20d5b626174c00e7fea59450a72dc56bcb97370c
Etko77 <etienne1800503@gmail.com>
2025-10-27 21:50:05 +0200

    Add changes

--- WebDev/ThymeLeafAdv/Task/src/main/java/com/company/web/springdemo/controllers/mvc/BeerMvcController.java
+++ WebDev/ThymeLeafAdv/Task/src/main/java/com/company/web/springdemo/controllers/mvc/BeerMvcController.java
@@ -1,26 +1,38 @@
 package com.company.web.springdemo.controllers.mvc;
 
 import com.company.web.springdemo.exceptions.EntityNotFoundException;
+import com.company.web.springdemo.helpers.BeerMapper;
 import com.company.web.springdemo.models.Beer;
+import com.company.web.springdemo.models.BeerDto;
 import com.company.web.springdemo.models.FilterOptions;
+import com.company.web.springdemo.models.User;
 import com.company.web.springdemo.services.BeerService;
+import com.company.web.springdemo.services.StyleService;
+import com.company.web.springdemo.services.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/beers")
 public class BeerMvcController {
 
     private final BeerService beerService;
+    private final UserService userService;
+    private final StyleService styleService;
+    private final BeerMapper beerMapper;
+
 
     @Autowired
-    public BeerMvcController(BeerService beerService) {
+    public BeerMvcController(BeerService beerService, UserService userService,
+                             StyleService styleService, BeerMapper beerMapper) {
         this.beerService = beerService;
+        this.userService = userService;
+        this.styleService = styleService;
+        this.beerMapper = beerMapper;
     }
 
     @GetMapping
@@ -41,5 +53,14 @@ public class BeerMvcController {
             return "ErrorView";
         }
     }
+    @PostMapping("/new")
+    public String createBeer(@Valid @ModelAttribute("beer") BeerDto beerDto, User user){
+        try{
+            beerService.create(beerMapper.fromDto(beerDto), user);
+            return "redirect:/beers";
+        }catch()
+
+    }
+
 
 }
